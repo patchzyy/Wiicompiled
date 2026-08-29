3605ecccf4337f4172722b348a87aeaa192b843f
jamie <git@skilet.ro>
2026-08-29 12:13:39 +0100

    refactor(nix): consolidate pipeline into build.nix, harden launcher

--- nix/rr-hash.nix
+++
@@ -1,9 +0,0 @@
-# Retro Rewind pack hash. Add the distribution zip to the store with:
-#
-#   nix-store --add-fixed sha256 --recursive RetroRewind6.zip
-#
-# then paste the printed hash below to enable the Retro Rewind product. An
-# empty string keeps the flake building the base product only.
-{
-  hash = "";
-}
--- nix/translator/default.nix
+++ nix/translator/default.nix
@@ -6,9 +6,9 @@
 buildDotnetModule rec {
   pname = "wiicompiled-translator";
   version = "0-unstable";
-  src = ../translator;
+  src = ../../translator;
   projectFile = "src/Translator.Cli/Translator.Cli.csproj";
-  nugetDeps = ./translator/nuget-deps.nix;
+  nugetDeps = ./nuget-deps.nix;
   dotnet-sdk = dotnet-sdk_8;
   executables = ["Translator.Cli"];
 
